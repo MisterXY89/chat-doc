47cabc302d5bb5234c3de15ade746e0889af40e2
Tilman Kerl <tilman.kerl@synyo.com>
2023-10-19 10:35:01 +0200

    first installs & pre-commit resort & fix

--- chat-doc/main.py
+++ chat-doc/main.py
@@ -4,6 +4,10 @@ MAIN FILE: --> ACEESIBLE & CALLED BY END USER
 also testing during dev
 """
 
+import os
+import sys
+
+from chatdoc.config import config
 
 if __name__ == "__main__":
-	print("loaded")
+    print("loaded")
