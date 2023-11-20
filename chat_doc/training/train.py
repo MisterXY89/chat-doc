31cb569dbe98716340182dc31bab1cbca0a91447
Tilman Kerl <tilmankerl@protonmail.com>
2023-11-20 20:16:35 +0100

    setup of training v2 + integreate run_clm train file

--- chat_doc/training/train.py
+++ chat_doc/training/train.py
@@ -1,3 +1,8 @@
+import time
+
+from huggingface_hub import HfFolder
+from sagemaker.huggingface import HuggingFace
+
 from chat_doc.config import logger
 from chat_doc.dataset_generation.dataset_factory import DatasetFactory
 from chat_doc.training.pre_training import PreTrainingProcessor
@@ -5,12 +10,50 @@ from chat_doc.training.setup_training import TrainingsSetup
 
 
 class Trainer:
-    def __init__(self, dataset_name, base_model, output_path):
+    def __init__(self, dataset_name, base_model, output_path, hyperparams):
         # dataset is name (icd, pmc, full)
         self.dataset_name = dataset_name
         self.base_model = base_model
         self.output_path = output_path
         self._is_initialized = False
+        self._hyperparameters(**hyperparams)
+
+    def _hyperparameters(self, epochs=3, per_device_train_batch_size=2, lr=2e-4):
+        # hyperparameters, which are passed into the training job
+        self.hyperparams = {
+            "model_id": self.base_model,  # pre-trained model
+            "dataset_path": "/opt/ml/input/data/training",  # path where sagemaker will save training dataset
+            "epochs": epochs,  # number of training epochs
+            "per_device_train_batch_size": per_device_train_batch_size,  # batch size for training
+            "lr": lr,  # learning rate used during training
+            "hf_token": HfFolder.get_token(),  # huggingface token to access llama 2
+            "merge_weights": True,  # wether to merge LoRA into the model (needs more memory)
+        }
+
+    def _hf_estimator(self, hyperparameters, job_name, role):
+        # create the Estimator
+        self.huggingface_estimator = HuggingFace(
+            entry_point="run_clm.py",  # train script
+            source_dir="scripts",  # directory which includes all the files needed for training
+            instance_type="ml.g5.4xlarge",  # instances type used for the training job
+            instance_count=1,  # the number of instances used for training
+            base_job_name=job_name,  # the name of the training job
+            role=self.trainings_config.role,  # Iam role used in training job to access AWS ressources, e.g. S3
+            volume_size=300,  # the size of the EBS volume in GB
+            transformers_version="4.28",  # the transformers version used in the training job
+            pytorch_version="2.0",  # the pytorch_version version used in the training job
+            py_version="py310",  # the python version used in the training job
+            hyperparameters=hyperparameters,  # the hyperparameters passed to the training job
+            environment={
+                "HUGGINGFACE_HUB_CACHE": "/tmp/.cache",  # set env variable to cache models in /tmp
+            },
+        )
+
+    def _build_training_job(self):
+        # define Training Job Name
+        job_name = f'huggingface-qlora-{time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())}'
+        logger.info(f"Training Job Name: {job_name}")
+        self._hf_estimator(self.hyperparams, job_name)
 
     def _initialize(self):
         logger.info("Loading dataset")
@@ -24,8 +67,11 @@ class Trainer:
         logger.info("Initializing trainer")
         self.trainings_config = TrainingsSetup()
         self.trainings_config.setup()
+        self.training_input_path = self.trainings_config.training_input_path(self.dataset_name)
 
         self.trainings_config.upload_data(self.train_set)
+
+        #
 
         logger.info("Initializing complete.")
         self._is_initialized = True
@@ -33,6 +79,12 @@ class Trainer:
     def train(self):
         if not self._is_initialized:
             self._initialize()
-        logger.info(f"Training model on dataset: {self.dataset}")
+
+        logger.info(f"Training model on dataset: {self.dataset_name}")
         logger.info(f"Base model: {self.base_model}")
         logger.info(f"Output path: {self.output_path}")
+
+        self._build_training_job()
+        data = {"training": self.training_input_path}
+        # start training job
+        self.huggingface_estimator.fit(data, wait=True)
